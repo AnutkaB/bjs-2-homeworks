19002b8a0711c00a79e46540b925b2b852afa436
Anna Bystrova <aabystrovy@mail.ru>
2024-01-23 14:17:56 +0300

    Second commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -18,33 +18,12 @@ function solveEquation(a, b, c) {
   return arr; // array
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let per = parseInt(percent) / 100 / 12; //процентная ставка
-    let con = parseInt(contribution); 	  //начальный взнос	
-    let am = parseInt(amount);  			  //общая стоимость	
-  	if (isNaN(per) || per < 0) {
-   		return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
-   	} else if (isNaN(con) || con < 0) {
-   		return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
-   	} else if (isNaN(am) || am < 0) {
-   		return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
-   	} else {
-			
-  		let today = new Date();
-  		if (today.getFullYear() > date.getFullYear()) {
-			return `Параметр "срок ипотеки" содержит неправильное значение ${date}`;
-		} else {
-			let s = am - con;						  //тело кредита
-  			let n = date.getMonth() - today.getMonth() + (12*(date.getFullYear() - today.getFullYear())); //срок кредита в месяцах
-  			let pay = s*(per+per/(((1+per)**n)-1));			//ежемесячная оплата
-  			let totalAmount = (pay * n).toFixed(2);
-  			console.log(totalAmount);
- 			return +totalAmount;
- 		}	
-    }
-
-  // код для задачи №2 писать здесь
-
-
-  return +totalAmount;
+function calculateTotalMortgage(percent, contribution, amount, countMonths)
+{
+ let totalAmount = 0;
+ let monthlyPercent = percent / 100 / 12;
+ let payment = amount - contribution;
+ let monthlyPayment = payment * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
+ totalAmount = monthlyPayment * countMonths;
+ return totalAmount.toFixed(2);
 }
